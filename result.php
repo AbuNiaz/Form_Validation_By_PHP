ddb77ad417dc3eb8535028b1980c277608f182b8
Abu Niaz <niaz35-507@diu.edu.bd>
2022-04-18 00:39:13 +0600

    Update result.php

--- result.php
+++ result.php
@@ -6,9 +6,17 @@ $pass = filter_input(INPUT_POST,'password');
 
 if(empty($name)){
     echo $name_error = 'Please Insert Your Name';
+}elseif(strlen($name) < 6){
+    echo $name_error = 'Name must have minimum 6 characters';
 }
+
+
 if(empty($pass)){
     echo $pass_error = 'Please Insert Your Name';
+}elseif(strlen($name) < 6){
+    echo $name_error = 'Name must have minimum 6 characters';
 }
+
+
 include("index.php");
 ?>
