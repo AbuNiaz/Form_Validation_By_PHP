c8e822b0d51eccbd046895a02ff0a199e31672f8
Abu Niaz <niaz35-507@diu.edu.bd>
2022-04-19 01:57:01 +0600

    Update db.php

--- db.php
+++ db.php
@@ -9,5 +9,12 @@ $conn = new sqli($severname, $username, $password);
 if($conn->connect_error){
     die("Connection failed :" .$conn->connect_error);
 }
+$sql = "CRETE DATABASE students";
+
+if($conn->query($sql)){
+   echo "Database Create Successfully" ;
+}else{
+    echo "Database Error";
+}
 
 ?>
