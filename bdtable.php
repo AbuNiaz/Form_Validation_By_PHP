b3834a2390490d1c6c1cb0caa03321933d1d7de7
Abu Niaz <niaz35-507@diu.edu.bd>
2022-04-20 00:05:47 +0600

    Update bdtable.php

--- bdtable.php
+++ bdtable.php
@@ -1,5 +1,23 @@
 <?php 
 
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "studens";
 
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  
+
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  
+  $sql = "CREATE TABLE MyGuests (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  firstname VARCHAR(30) NOT NULL,
+  lastname VARCHAR(30) NOT NULL,
+  email VARCHAR(50),
+  reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  )";
 
 ?>
