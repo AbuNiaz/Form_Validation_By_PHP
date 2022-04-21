daec963a70c371562114270cd02f83dfcb7900ce
Abu Niaz <niaz35-507@diu.edu.bd>
2022-04-21 12:09:28 +0600

    Update tbinsert.php

--- tbinsert.php
+++ tbinsert.php
@@ -5,4 +5,11 @@ $username = "username";
 $password = "password";
 $dbname = "Students";
 
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
 ?>
